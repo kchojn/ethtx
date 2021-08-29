d79e1944bed1c74eb71db48220be87ce54d214b2
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-08-29 16:49:30 +0200

    init async mongo

---
+++ ethtx/db.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class MotorBase:
+    _db = {}
+    _collection = {}
+
+    def __init__(self, connection_string: str, loop=None):
+        self.connection_string = connection_string
+        self.loop = loop or asyncio.get_event_loop()
+
+    def client(self):
+        return AsyncIOMotorClient(self.connection_string, io_loop=self.loop)
+
+    def get_db(self, db: str):
+        """
+        Get a db instance
+        :param db: database name
+        :return: the motor db instance
+        """
+        return self.client()[db]
