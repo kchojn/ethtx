3c21e73a4b95ebf3081d2fdeef29463430198fae
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-08-30 13:59:20 +0200

    update empty methods

--- ethtx/events/observer/event_publisher.py
+++ ethtx/events/observer/event_publisher.py
@@ -1,13 +1,20 @@
+from typing import List
+
 from ethtx.events.observer.observer_abc import Observer
 from ethtx.events.observer.subject_abc import Subject
 
 
 class EventSubject(Subject):
+    _current_event_state: str = None
+
+    _observers: List[Observer] = []
+
     def attach(self, observer: Observer) -> None:
-        pass
+        self._observers.append(observer)
 
     def detach(self, observer: Observer) -> None:
-        pass
+        self._observers.remove(observer)
 
     def notify(self, observer: Observer) -> None:
-        pass
+        for observer in self._observers:
+            observer.update(self)
