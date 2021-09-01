65c3898b0b06c01d7e8e267b747209637785e655
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-09-01 14:15:47 +0200

    fix error

--- ethtx/events/observer/observer_abc.py
+++ ethtx/events/observer/observer_abc.py
@@ -4,9 +4,6 @@ from abc import ABC, abstractmethod
 class Observer(ABC):
     """ The Observer interface declares the update method, used by subjects. """
 
-    def __repr__(self) -> str:
-        return self.__str__()
-
     @abstractmethod
     def update(self, subject, *args, **kwargs) -> None:
         """ Receive update from subject. """
