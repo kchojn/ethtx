a9d82879e91999f81e64010e3239399f55e2d3bb
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-09-02 15:53:12 +0200

    add debug

--- ethtx/events/observer/event_subscribers.py
+++ ethtx/events/observer/event_subscribers.py
@@ -1,8 +1,12 @@
+import logging
+
 from ethtx.events.models.abi import ABIModel
 from ethtx.events.models.semantic import SemanticModel
 from ethtx.events.models.transaction import TransactionModel, FullTransactionModel
 from ethtx.events.observer.event_publisher import EventSubject
 from ethtx.events.observer.observer_abc import Observer
+
+log = logging.getLogger(__name__)
 
 
 class GlobalEventObserver(Observer):
@@ -19,6 +23,7 @@ class GlobalEventObserver(Observer):
             if "hash" in kwargs:
                 self.event.hash = kwargs["hash"]
                 subject.emit_event(event=self.event)
+                log.debug("%s emitted event: %s", self.__class__.__name__, self.event)
 
 
 class TransactionEventObserver(Observer):
@@ -34,17 +39,23 @@ class TransactionEventObserver(Observer):
         ):
             if "starts" in kwargs:
                 self.event.starts = kwargs["starts"]
+
             if "ends" in kwargs:
                 self.event.ends = kwargs["ends"]
                 subject.emit_event(event=self.event)
+                log.debug("%s emitted event: %s", self.__class__.__name__, self.event)
+
             if "address_semantics" in kwargs:
                 self.event.meta.address_semantics.append(kwargs["address_semantics"])
+
             if "signature_semantics" in kwargs:
                 self.event.meta.signature_semantics.append(
                     kwargs["signature_semantics"]
                 )
+
             if "exception" in kwargs:
                 self.event.exception = kwargs["exception"]
+
             if "message" in kwargs:
                 self.event.message = kwargs["message"]
 
@@ -62,11 +73,15 @@ class ABIEventObserver(Observer):
         ):
             if "starts" in kwargs:
                 self.event.starts = kwargs["starts"]
+
             if "ends" in kwargs:
                 self.event.ends = kwargs["ends"]
                 subject.emit_event(event=self.event)
+                log.debug("%s emitted event: %s", self.__class__.__name__, self.event)
+
             if "exception" in kwargs:
                 self.event.exception = kwargs["exception"]
+
             if "message" in kwargs:
                 self.event.message = kwargs["message"]
 
@@ -84,10 +99,14 @@ class SemanticsEventObserver(Observer):
         ):
             if "starts" in kwargs:
                 self.event.starts = kwargs["starts"]
+
             if "ends" in kwargs:
                 self.event.ends = kwargs["ends"]
                 subject.emit_event(event=self.event)
+                log.debug("%s emitted event: %s", self.__class__.__name__, self.event)
+
             if "exception" in kwargs:
                 self.event.exception = kwargs["exception"]
+
             if "message" in kwargs:
                 self.event.message = kwargs["message"]
